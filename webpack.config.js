e5157fac9d2aa28eb920a55e7a48a0b9ed8b923e
EllaGoncharenko <EllaGoncharenko@ukr.net>
2018-12-13 17:25:14 +0200

    Create commit webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,14 @@ const path = require('path');
 
 module.exports = {
     entry: './src/app.js',
+    mode: 'development',
     output: {
         path: path.resolve(__dirname, 'public','dist'),
         filename: 'bundle.js'
-    }
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+    },
+
+    devtool: 'cheap-eval-source-map' // remove for build
 };
